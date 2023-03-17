f7ff906c5c3395e803973732712b89538fd6a2b3
Dharshini <95228786+Dharshini231001@users.noreply.github.com>
2023-03-17 14:57:28 +0530

    Add files via upload

---
+++ lib/data/category_model.dart
@@ -0,0 +1,14 @@
+class Category {
+  final int id;
+  final String name;
+
+  Category(this.id, this.name);
+}
+
+List<Category> categories = [
+  Category(0, 'All'),
+  Category(1, 'Villas'),
+  Category(2, 'Apartments'),
+  Category(3, 'Office & Studio'),
+  Category(4, 'Plots'),
+];
---
+++ lib/data/plant_data.dart
@@ -0,0 +1,77 @@
+import 'package:ui_13/data/plant_model.dart';
+
+List<Plants> plants = [
+  Plants(
+    id: 0,
+    name: 'Dharshini Lands',
+    imagePath: 'assets/images/image1.jpg',
+    category: 'Plots',
+    description:
+        '2.40 acres main road property sale. Near annur. Road face 240 feet. Annur to coimbatore nh main road property. Clear document title. Site valuation land. More details contact.',
+    price: 100000,
+    isFavorit: true,
+  ),
+  Plants(
+    id: 1,
+    name: 'Eden valley',
+    imagePath: 'assets/images/image2.jpg',
+    category: 'Apartments',
+    description:
+        'The apartment consists of a large bright bedroom with a comfy king-sized bed, a modern fully-equipped kitchen and a sunlit living room with Apple TV and free Netflix account. It is the perfect place to stay for couples looking for a romantic location in the historic centre, within walking distance of some of the most beautiful sceneries you can find in the city and all famous landmarks. The sofa in the living room can also serve as an additional bed for a 3rd guest. Located in a side street between the ___ river bank and the leafy ___ hill park, the apartment is very quiet at night. Please note, my apartment is on the 3rd floor with no elevator. I am more than happy to help you with your luggage! ',
+    price: 2000000,
+    isFavorit: false,
+  ),
+  Plants(
+    id: 2,
+    name: 'Aadhi Builders',
+    imagePath: 'assets/images/image3.jpg',
+    category: 'Office & Studio',
+    description:
+        'This exclusive villa is located in an interesting building at the ground floor. With an area of 38 square meters when you come in, you find yourself in a spacious royal studio equipped with nice comfortable sofa bed, royal king size bed, flat screen TV and wardrobe, luxury comfort dinning table for 4 persons and fully equipped kitchen.',
+    price:300000,
+    isFavorit: false,
+  ),
+  Plants(
+    id: 3,
+    name: 'Premiere Property',
+    imagePath: 'assets/images/image5.jpg',
+    category: 'Villas',
+    description:
+        'This exclusive office is located in an interesting building at the ground floor. With an area of 38 square meters when you come in, you find yourself in a spacious royal studio equipped with nice comfortable sofa bed, royal king size bed, flat screen TV and wardrobe, luxury comfort dinning table for 4 persons and fully equipped kitchen.',
+    price: 3000000,
+    isFavorit: false,
+  ),
+];
+
+List<Plants> populerPlants = [
+  Plants(
+    id: 0,
+    name: 'Dhivya Lands',
+    imagePath: 'assets/images/01.png',
+    category: 'Plots',
+    description:
+        '2.40 acres main road property sale. Near annur. Road face 240 feet. Annur to coimbatore nh main road property. Clear document title. Site valuation land. More details contact.',
+    price: 200000,
+    isFavorit: false,
+  ),
+  Plants(
+    id: 1,
+    name: 'Amkin Apartment',
+    imagePath: 'assets/images/02.png',
+    category: 'Apartments',
+    description:
+        'The apartment consists of a large bright bedroom with a comfy king-sized bed, a modern fully-equipped kitchen and a sunlit living room with Apple TV and free Netflix account. It is the perfect place to stay for couples looking for a romantic location in the historic centre, within walking distance of some of the most beautiful sceneries you can find in the city and all famous landmarks. The sofa in the living room can also serve as an additional bed for a 3rd guest. Located in a side street between the ___ river bank and the leafy ___ hill park, the apartment is very quiet at night. Please note, my apartment is on the 3rd floor with no elevator. I am more than happy to help you with your luggage! ',
+    price: 5000000,
+    isFavorit: false,
+  ),
+  Plants(
+    id: 2,
+    name: 'Eden valley',
+    imagePath: 'assets/images/03.png',
+    category: 'Apartments',
+    description:
+        'The apartment consists of a large bright bedroom with a comfy king-sized bed, a modern fully-equipped kitchen and a sunlit living room with Apple TV and free Netflix account. It is the perfect place to stay for couples looking for a romantic location in the historic centre, within walking distance of some of the most beautiful sceneries you can find in the city and all famous landmarks. The sofa in the living room can also serve as an additional bed for a 3rd guest. Located in a side street between the ___ river bank and the leafy ___ hill park, the apartment is very quiet at night. Please note, my apartment is on the 3rd floor with no elevator. I am more than happy to help you with your luggage!  ',
+    price: 60000,
+    isFavorit: false,
+  ),
+];
